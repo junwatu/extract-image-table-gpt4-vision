eae9592a118e31c3ba514403d11849586511cbfb
Equan P <equan.p@gmail.com>
2023-12-22 15:43:09 +0700

    Fix import and update port number

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-import Express from 'express';
+import express from 'express';
 import OpenAI from 'openai';
 import 'dotenv/config';
 
-const app = Express();
+const app = express();
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
@@ -24,17 +24,27 @@ async function processImageRequest(imageUrl) {
 	return response.choices[0];
 }
 
+app.get('/', (req, res) => {
+	const jsonData = {
+		urls: {
+			home: '/',
+			processImage: '/process-image'
+		},
+	}
+	res.json(jsonData);
+});
+
 app.post('/process-image', async (req, res) => {
 	try {
-		const imageUrl = req.body.imageUrl; // assuming the image URL is sent in the request body
-		const result = await processImageRequest(imageUrl);
+		const imageUrl = req.body.imageData;
+		const result = await processImageRequest(imageData);
 		res.json(result);
 	} catch (error) {
 		res.status(500).send('Error processing image request');
 	}
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5115;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
